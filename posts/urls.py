ada70dc3a688563ca41f4f53d91427059d779163
qone2 <whrbdnjs33@naver.com>
2021-09-16 18:49:46 +0900

    버그 수정

--- posts/urls.py
+++ posts/urls.py
@@ -4,8 +4,8 @@ from posts import views
 urlpatterns = [
     path('posts', views.post_list),
     path('post/<int:pk>', views.post_detail),
-    path('posts/<string:keyword>', views.post_list_keyword_query),
-    path('posts/<string:keyword>/<float:food_score>', views.post_list_keyword_score_query),
+    path('posts/<str:keyword>', views.post_list_keyword_query),
+    path('posts/<str:keyword>/<int:food_score>', views.post_list_keyword_score_query),
     path('keywords', views.keyword_list),
-    path('post/<string:keyword>/<string:post_id>', views.post_postid_keyword_query)
+    path('post/<str:keyword>/<str:post_id>', views.post_postid_keyword_query)
 ]
