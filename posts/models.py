ec07482c08b0be7428b41d621d49c6752ec0c589
qone2 <whrbdnjs33@naver.com>
2021-09-18 02:51:01 +0900

    db index 재조정

--- posts/models.py
+++ posts/models.py
@@ -5,9 +5,9 @@ class Post(models.Model):
     post_id = models.CharField(max_length=11, blank=False)
     post_url = models.CharField(max_length=100, blank=False)
     img_url = models.CharField(max_length=100, blank=False)
-    keyword = models.CharField(max_length=50, blank=False)
+    keyword = models.CharField(max_length=50, blank=False, db_index=True)
     food_score = models.FloatField(default=0)
-    scraped_date = models.DateTimeField(auto_now_add=True)
+    scraped_date = models.DateTimeField(auto_now_add=True, db_index=True)
     post_text = models.TextField(default="", blank=True)
     insta_analysis = models.TextField(default="", blank=True)
     insta_analysis_food = models.BooleanField(default=False)
@@ -16,7 +16,4 @@ class Post(models.Model):
     class Meta:
         constraints = [
             models.UniqueConstraint(fields=["post_id", "keyword"], name="unique post and keyword")
-        ]
-        indexes = [
-            models.Index(fields=["keyword"])
         ]
