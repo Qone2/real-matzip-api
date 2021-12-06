a735fef96dcd76210f528183d7b42465aa4c7979
qone2 <whrbdnjs33@naver.com>
2021-12-06 19:25:16 +0900

    함수설명 추가

--- posts/views.py
+++ posts/views.py
@@ -11,7 +11,13 @@ from rest_framework import generics
 @api_view(['GET', 'POST'])
 def post_list(request):
     """
+    GET
     모든 포스트목록을 불러옵니다.
+
+    POST
+    포스트를 저장할 때 사용합니다. json형식을 담아 요청합니다.
+    또한 스크랩 했을때 스크랩 결과물이 0개 일때도 사용합니다.
+    이때는 not-scraped-yet 목록에 나오지 않게 하는 목적입니다.
     """
     if request.method == 'GET':
         posts = Post.objects.all().order_by("-scraped_date")
@@ -29,7 +35,7 @@ def post_list(request):
 @api_view(['GET', 'PUT', 'DELETE'])
 def post_detail(request, pk):
     """
-    Retrieve, update or delete.
+    id에 해당하는 포스트를 반환하거나, 수정하거나, 삭제합니다.
     """
     try:
         post = Post.objects.get(pk=pk)
@@ -54,6 +60,10 @@ def post_detail(request, pk):
 
 @api_view(['GET'])
 def post_list_keyword_query(request, keyword):
+    """
+    키워드로 스크랩 된 모든 포스트들을 반환합니다.
+    scraped_date 최신순으로 보여집니다.
+    """
     posts = Post.objects.filter(keyword=keyword).values().order_by("-scraped_date")
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
@@ -61,6 +71,10 @@ def post_list_keyword_query(request, keyword):
 
 @api_view(['GET'])
 def post_list_keyword_query_posted_date_order(request, keyword):
+    """
+    키워드로 스크랩 된 모든 포스트들을 반환합니다.
+    posted_date 최신순으로 보여집니다.
+    """
     posts = Post.objects.filter(keyword=keyword).values().order_by("-posted_date")
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
@@ -68,6 +82,10 @@ def post_list_keyword_query_posted_date_order(request, keyword):
 
 @api_view(['GET'])
 def post_list_keyword_score_query(request, keyword, food_score):
+    """
+    키워드로 스크랩 된 모든 포스트들 중에서 음식점수가 원하는 값보다 높은 포스트들만 보여집니다.
+    scraped_date 최신순으로 보여집니다.
+    """
     posts = Post.objects.filter(keyword=keyword, food_score__gte=float(food_score), is_ad=False).values().order_by("-scraped_date")
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
@@ -75,6 +93,10 @@ def post_list_keyword_score_query(request, keyword, food_score):
 
 @api_view(['GET'])
 def post_list_keyword_score_query_posted_date_order(request, keyword, food_score):
+    """
+    키워드로 스크랩 된 모든 포스트들 중에서 음식점수가 원하는 값보다 높은 포스트들만 보여집니다.
+    posted_date 최신순으로 보여집니다.
+    """
     posts = Post.objects.filter(keyword=keyword, food_score__gte=float(food_score), is_ad=False).values().order_by("-posted_date")
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
@@ -85,6 +107,11 @@ class PostListKeywordScoreQuery(generics.ListAPIView):
     pagination_class = PageNumberPagination
 
     def get_queryset(self):
+        """
+        키워드로 스크랩 된 모든 포스트들 중에서 음식점수가 원하는 값보다 높은 포스트들만 보여집니다.
+        페이징이 적용되어 보여집니다.
+        scraped_date 최신순으로 보여집니다.
+        """
         keyword = self.kwargs.get("keyword", None)
         food_score = self.kwargs.get("food_score", None)
         queryset = Post.objects.filter(keyword=keyword, food_score__gte=float(food_score), is_ad=False).values().order_by(
@@ -94,6 +121,11 @@ class PostListKeywordScoreQuery(generics.ListAPIView):
 
 @api_view(['GET'])
 def keyword_list(request):
+    """
+    한번이상 스크랩된 키워드 목록을 보여줍니다.
+    포스트 개수가 2개 이상인 키워드 목록을 보여줍니다.
+    각각의 키워드에 대하여, 등록된 날짜가 가장 오래된(처음등록된) 포스트의 날짜가 최신순으로 정렬되어 보여집니다.
+    """
     keywords = Post.objects.values("keyword").annotate(post_count=Count("post_id"), first_date=Min("scraped_date")).filter(post_count__gt=1).order_by("first_date")
     response = {"keyword_list": []}
     for keyword in keywords:
@@ -103,6 +135,9 @@ def keyword_list(request):
 
 @api_view(['GET', 'DELETE'])
 def post_postid_keyword_query(request, keyword, post_id):
+    """
+    키워드와 post_id에 해당하는 포스트를 반환하거나, 삭제합니다.
+    """
     if request.method == 'GET':
         try:
             post = Post.objects.get(post_id=post_id, keyword=keyword)
@@ -122,6 +157,13 @@ def post_postid_keyword_query(request, keyword, post_id):
 
 @api_view(['GET'])
 def not_scraped_yet(request):
+    """
+    아직 스크랩되지 않은 키워드 목록을 보여줍니다.
+    정확히는 키워드별로 포스트개수가 1개인 키워드 목록을 반환합니다.
+    해당 서버는 키워드별로 포스트 개수가 1개 이상인 키워드를 스크랩 대사으로 간주합니다.
+    또한 키워드별로 포스트 개수가 1개인 키워드는 아직 스크랩되지 않은 키워드로 판단합니다.
+    각각의 키워드에 대하여, 등록된 날짜가 가장 오래된(처음등록된) 포스트의 날짜가 최신순으로 정렬되어 보여집니다.
+    """
     response = {"keyword_list": []}
     keyword_list = Post.objects.values("keyword").annotate(post_count=Count("post_id"), first_date=Min("scraped_date")).filter(post_count__lte=1).order_by("first_date")
     for keyword in keyword_list:
@@ -131,6 +173,11 @@ def not_scraped_yet(request):
 
 @api_view(['POST'])
 def insert_keyword(request, keyword):
+    """
+    키워드로 스크랩 된 포스트가 하나도 없을 때 사용합니다. 미리 지정된 형식의 더미포스트가 저장됩니다.
+    저희 서버에서는 특정키워드에 포스트가 하나밖에 없으면 스크랩대상이지만 아직 스크랩되지 않은 걸로 간주합니다.
+    따라서 해당 주소로 요청을 보내면 곧 해당 키워드로 스크랩하게 됩니다.
+    """
     if "맛집" not in keyword:
         return Response(status=status.HTTP_400_BAD_REQUEST)
     data = {
@@ -153,6 +200,11 @@ def insert_keyword(request, keyword):
 
 @api_view(['GET'])
 def all_keyword_list(request):
+    """
+    포스트개수와 상관없이 모든 키워드목록을 반환합니다.
+    스크랩할 계정을 인덱싱할 때 사용합니다.
+    각각의 키워드에 대하여, 등록된 날짜가 가장 오래된(처음등록된) 포스트의 날짜가 최신순으로 정렬되어 보여집니다.
+    """
     keywords = Post.objects.values("keyword").annotate(first_date=Min("scraped_date")).order_by("first_date")
     response = {"keyword_list": []}
     for keyword in keywords:
@@ -162,6 +214,9 @@ def all_keyword_list(request):
 
 @api_view(['GET'])
 def all_keyword_list_alphabetical_order(request):
+    """
+    모든 키워드를 알파벳순서로 보여줍니다.
+    """
     keywords = Post.objects.values("keyword").distinct()
     response = {"keyword_list": []}
     for keyword in keywords:
@@ -170,6 +225,10 @@ def all_keyword_list_alphabetical_order(request):
 
 
 class AllPostList(generics.ListAPIView):
+    """
+    모든 포스트목록을 불러옵니다.
+    페이징이 적용되어 반환됩니다.
+    """
     queryset = Post.objects.all().order_by("-scraped_date")
     serializer_class = PostSerializer
     pagination_class = PageNumberPagination
